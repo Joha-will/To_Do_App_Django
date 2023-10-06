1820d426d75f2533a3a0772558d8b6cb2657c965
Joha-will <johawilsonatu@icloud.com>
2023-10-06 21:38:30 +0100

    create task model and register admin

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!41h65l#$jeqejj4ik!it*@%s)pix#!5!&7%xm@%dh^kau$-$$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_list',
 ]
 
 MIDDLEWARE = [
---
+++ todo_list/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Task(models.Model):
+
+    task = models.CharField(max_length=254,)
+    is_completed = models.BooleanField(default=False)
+    create_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.task
