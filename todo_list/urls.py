9debe80fc289be957c17156d031f36fa3b843e08
Joha-will <johawilsonatu@icloud.com>
2023-10-17 17:32:29 +0100

    Create edit task view, url and templates

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -2,6 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # Add task Url
     path('add_task/', views.add_task, name='add_task'),
+    # Mark as done Url
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    # Mark as undone Url
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # Edit task Url
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
 
 ]
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,9 +1,41 @@
-from django.shortcuts import render, redirect
+from multiprocessing import context
+from django.shortcuts import render, redirect, get_object_or_404
 from . models import Task
 
 
 def add_task(request):
-    """ This view gives users the ability to at tasks """
+    """ This view gives users the ability to add tasks """
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+
+def mark_as_done(request, pk):
+    """ This view gives users the ability mark tasks as done """
+
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+
+def mark_as_undone(request, pk):
+    """ This view gives users the ability undo tasks """
+
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+
+def edit_task(request, pk):
+    """ This view gives users the ability edit tasks """
+
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        return
+    else:
+        context = {
+            'get_task': get_task,
+        }
+    return render(request, 'edit_task.html', context)
