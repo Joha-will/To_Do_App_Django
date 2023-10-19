b59c49aa2f8c3b556cad4d14cccd6dacf8a2c098
Joha Wilson <98041941+Joha-will@users.noreply.github.com>
2023-10-19 19:35:51 +0000

    Adjust allowed_host

--- to_do_app/settings.py
+++ to_do_app/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['todo-app-djang0-79049661daf2.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['todo-app-djang0-79049661daf2.herokuapp.com/', 'localhost']
 
 # Application definition
 
